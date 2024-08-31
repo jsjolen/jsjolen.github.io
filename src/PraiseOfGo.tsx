39b4fa00853c33103ca6bfa3c9a400213ba806ef
Johan Sjölén <johan.borglin.sjolen@gmail.com>
2024-08-31 14:13:56 +0200

    Delete outdated stuff

--- src/PraiseOfGo.tsx
+++
@@ -1,9 +0,0 @@
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-import React, { FunctionComponent, ReactElement } from 'react';
-
-export function PraiseOfGo() {
-  return (
-    <p> Hi </p>
-    );
-}
